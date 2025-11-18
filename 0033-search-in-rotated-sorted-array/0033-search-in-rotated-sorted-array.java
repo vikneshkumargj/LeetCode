d9c9f5da6f23c95c64b405b23257f72476c716be
VikneshKumar <vikneshkumargj@gmail.com>
2025-11-18 18:23:09 +0530

    Time: 0 ms (100%), Space: 43.7 MB (13.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int peak = findpivot(nums,target);
-        if(peak==-1)
+        int pivot = findpivot(nums);
+        if(pivot==-1)
         {
             return binarysearch(nums,target,0,nums.length-1);
         }
-        else if(peak!=-1)
+        else
         {
-            if(nums[peak]==target)
+            if(nums[pivot]==target)return pivot;
+            if(nums[0]>target)
             {
-                return peak;
+                return binarysearch(nums,target,pivot+1,nums.length-1);
             }
-            else if(nums[0]>target)
+            else
             {
-                return binarysearch(nums,target,peak+1,nums.length-1);
-            }
-            else {
-                return binarysearch(nums,target,0,peak-1);
+                return binarysearch(nums,target,0,pivot-1);
             }
         }
-        return -1;
+        
     }
-    static int findpivot(int[] arr,int target)
+    
+    static int findpivot(int[] arr )
     {
         int start = 0;
         int end = arr.length-1;
@@ -32,7 +31,7 @@ class Solution {
             {
                 return mid;
             }
-            else if(mid>start && arr[mid]<arr[mid-1])
+            else if(start<mid && arr[mid]<arr[mid-1])
             {
                 return mid-1;
             }
@@ -40,14 +39,18 @@ class Solution {
             {
                 end=mid-1;
             }
-            else {
+            else if(arr[mid]>=arr[start])
+            {
                 start=mid+1;
             }
+            
         }
         return -1;
     }
 
-    static int binarysearch(int[] arr , int target , int start , int end)
+
+
+    static int binarysearch(int[] arr,int target,int start , int end)
     {
         while(start<=end)
         {
@@ -56,7 +59,7 @@ class Solution {
             {
                 end=mid-1;
             }
-            else if(arr[mid]<target)
+            else if (arr[mid]<target)
             {
                 start=mid+1;
             }
