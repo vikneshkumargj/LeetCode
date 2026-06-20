01924157bf4a7a2fe076fe539baf6a05ab2a459e
VikneshKumar <vikneshkumargj@gmail.com>
2026-06-20 10:56:21 +0530

    Time: 3 ms (87.57%), Space: 43.6 MB (15.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {   
+            char c = s.charAt(i);
+            if(c=='{'||c=='('||c=='[')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if(st.isEmpty()){return false;}
+                if((c=='}') && (st.peek()=='{'))
+                {
+                    st.pop();
+                }
+                else if((c==']') && (st.peek()=='['))
+                {
+                    st.pop();
+                }
+                else if((c==')') && (st.peek()=='('))
+                {
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.isEmpty()){return true;}
+        return false;
+    }
+}
